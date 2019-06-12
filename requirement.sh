f29340ae01aa016b37dd51a357d658add7f7ae65
RunningHorse <2412546652@qq.com>
2019-06-12 14:37:29 +0800

    requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,10 @@
+pip install tqdm
+pip install email
+pip install codecs
+pip install re
+pip install bs4
+pip install tensorflow
+pip install keras
+pip install nltk
+pip install joblib
+pip install kears
